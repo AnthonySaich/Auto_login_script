e24832e0088449280980069a1d32cc1c4439834b
Anthony Saich <31777044+AnthonySaich@users.noreply.github.com>
2020-10-18 22:57:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ var passoword = fs.readFileSync('sececrt.text').toString();
     
     //The scripts tries to go to the my-rota page frist. But it redirect to the login page
     //login in using username and password
-    console.log('here1');
+    console.log('Found the website URL');
     await page.waitForSelector('[type=text]');
-    console.log('here2');
     await page.type('[type=text]','Emplyee Number ');
-    console.log('here3');
     //The password is varble from the secert.text
     await page.type('[type=password]',passoword);
     await page.click('[type=submit]');
+    console.log('Submitted');
     //This clicks the submit button
     
     
